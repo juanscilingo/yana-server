cf7f83c89bc2c6eed5fd64ae567a0b9638fd80bf
Juan Manuel Scilingo <juanscilingo@gmail.com>
2019-03-09 07:08:36 -0300

    Adds mongo connection and session configuration

--- src/index.js
+++ src/index.js
@@ -1,26 +1,72 @@
 import dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import session from "express-session";
+import connectMongo from "connect-mongo";
 import configureRoutes from "./routes";
 
 dotenv.config();
 
-const { APP_PORT } = process.env;
+const {
+  APP_PORT,
+  NODE_ENV,
+  DB_HOST,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  SESSION_LIFETIME = 1000 * 60 * 60 * 2, // 2 hours
+  SESSION_NAME,
+  SESSION_SECRET
+} = process.env;
+const IN_PROD = NODE_ENV === "production";
 
-// EXPRESS SERVER
-const app = express();
+(async () => {
+  try {
+    // DB CONNECTION
+    const CONNECTION_STRING = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
-// HEADER CONFIGURATION
-app.disable("x-powered-by");
+    console.log(`Attempting to connect to MongoDB: ${CONNECTION_STRING}`);
+    await mongoose.connect(CONNECTION_STRING, {
+      useNewUrlParser: true
+    });
 
-// MIDDLEWARE
-app.use(cors({ credentials: true }));
-app.use(bodyParser.json());
+    // EXPRESS SERVER
+    const app = express();
 
-// ROUTES
-configureRoutes(app);
+    // HEADER CONFIGURATION
+    app.disable("x-powered-by");
 
-app.listen({ port: APP_PORT }, () =>
-  console.log(`Server ready at http://localhost:${APP_PORT}`)
-);
+    // SESSION
+    const MongoStore = connectMongo(session);
+    app.use(
+      session({
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        name: SESSION_NAME,
+        secret: SESSION_SECRET,
+        resave: true,
+        rolling: true,
+        saveUninitialized: false,
+        cookie: {
+          maxAge: parseInt(SESSION_LIFETIME),
+          sameSite: true,
+          secure: IN_PROD
+        }
+      })
+    );
+
+    // MIDDLEWARE
+    app.use(cors({ credentials: true }));
+    app.use(bodyParser.json());
+
+    // ROUTES
+    configureRoutes(app);
+
+    app.listen({ port: APP_PORT }, () =>
+      console.log(`Server ready at http://localhost:${APP_PORT}`)
+    );
+  } catch (e) {
+    console.error(e);
+  }
+})();
